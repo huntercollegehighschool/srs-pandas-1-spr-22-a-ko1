e09e15d22e60fda69d8de0d623af97dc9a13d908
Ayan Kohli <79936654+a-ko1@users.noreply.github.com>
2022-03-11 17:28:55 +0000

    CODE

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ We'll be using the pandas (short for panels of data) module to analyze a large d
 1. The conventional way to import the pandas module is below. Note that the "as pd" part is optional (but widely used).
 import pandas as pd
 """
-
+import pandas as pd
 
 """
 2. This folder contains a large .csv (spreadsheet) file named ign.csv. Import the data stored in the csv as a dataframe to Python and store it in a variable. Try printing the data.
 pd.read_csv('<filename/url>')
 """
+x = pd.read_csv('ign.csv')
 
 
 """
@@ -20,22 +21,25 @@ pd.read_csv('<filename/url>')
 Change the dataframe so that the index are not integers starting from zero, but instead the title of the game. You'll have to go to the .csv file to find in which column the game titles are.
 pd.read_csv('<filename/url>', index_col = <column number>)
 """
+y = pd.read_csv('ign.csv', index_col=2)
 
 
 """
 4. Use <dataframe>.index and print the result to see a list of indices/row names. (Note: the console will not print all of them)
 """
-
+z = y.index
 
 """
 5. Use <dataframe>.columns and print the result to see a list of column names.
 """
+q = y.columns
 
 """
 6. In a pandas dataframe, columns are indexed by column name. For example, reviews['score_phrase'] returns a dataframe that contains only the index (the game title) and the score phrase given to that game.
 
 Create a dataframe that only contains the score for each game. You can save it in a new dataframe variable, but you don't have to. Print that new dataframe.
 """
+w = y['score']
 
 
 """
@@ -46,19 +50,32 @@ Create a dataframe that only contains the score for each game. You can save it i
 <dataframe>.median()
 <dataframe>.std()  # standard deviation
 """
+mox = w.max()
+mon = w.min()
+moon = w.mean()
+moodian = w.median()
+devo = w.std()
+
 
 """
 8. pandas has a function that returns a series indicating how many of each value is in a dataframe (<dataframe>.value_counts()). Use that function to find the counts in the platform, score, and release_month columns, and print the results. You will want to create 3 new dataframes that you can use value_counts() on.
 """
+plotform = x['platform']
+scorr = x['score']
+moonth = x['release_month']
+
 
 """
 9. pandas supports Boolean indexing. For example, reviews[reviews['score'] < 1] would return a dataframe containing games that got a score below a 1.
 
 Create 2 new dataframes, one with only games that got scores below 2, and one with only games that got scores of 10. Print both dataframes.
 """
+scorr_und_2 = x[x['score'] < 2]
+perf_scorr = x[x['score'] == 10]
 
 """
 10. Save both the new dataframes you created in #9 to a csv file (it will make it much nicer to look at). Name the files 'trash.csv' and 'perfect.csv'
 <dataframe>.to_csv('<filename>')
 """
-
+scorr_und_2.to_csv('trash')
+perf_scorr.to_csv('perfect')
